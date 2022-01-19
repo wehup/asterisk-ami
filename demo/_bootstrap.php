44919b4168be1135a0b89278106098c53694fe2a
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2022-01-19 16:00:14 -0300

    Updated composer autoload include

--- demo/_bootstrap.php
+++ demo/_bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'vendor/autoload.php';
+include '../../../autoload.php';
 
 $loginRequest = new Wehup\AMI\Request\LoginRequest('admin', 'admin');
 $manager = new Wehup\AMI\Manager('127.0.0.1', 8088, 'asterisk', $loginRequest);
