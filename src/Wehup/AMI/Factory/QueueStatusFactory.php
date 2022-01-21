5bc4ba81677d165da95cdc2c47111bf699a686db
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2022-01-21 09:59:32 -0300

    Added QueueStatus factory

---
+++ src/Wehup/AMI/Factory/QueueStatusFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Wehup\AMI\Factory;
+
+use Wehup\AMI\Response;
+use Wehup\AMI\Exception;
+
+class QueueStatusFactory implements FactoryInterface
+{
+
+    protected $request;
+
+    public function __construct(\Wehup\AMI\Request\RequestInterface $request)
+    {
+        $this->request = $request;
+    }
+
+    public function createResponse(\Guzzle\Http\Message\Response $response)
+    {
+        $body = $response->getBody(true);
+
+        if (preg_match('#^Response: Error\r\nMessage: Permission denied#', $body)) {
+            return new Response\PermissionDeniedResponse();
+        }                
+
+        if (preg_match('#^Response: Success\r\n#', $body)) {
+            $response = new Response\QueueStatusResponse();
+
+            $data = explode("\r\n\r\n", $body);            
+
+            array_shift($data);
+            array_pop($data);
+
+            foreach ($data as $row) {
+                if (preg_match('#^Event: QueueParams\r\n#', $row)) $response->addQueue($row);                                    
+                if (preg_match('#^Event: QueueMember\r\n#', $row)) $response->addMember($row);                                    
+                if (preg_match('#^Event: QueueEntry\r\n#', $row)) $response->addCaller($row);                                    
+            }
+
+            return $response;
+        }
+
+        throw new Exception\UnexpectedResponseException($body);
+    }
+
+}
